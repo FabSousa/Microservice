9bb56ca218c82469950baf0f674e088fe5b149e0
Fábio Sousa <73472338+FabSousa@users.noreply.github.com>
2023-05-22 10:21:28 -0300

    aula 2205 (update)

--- Clientes-Produtos-rest-api/src/main/java/com/fiap/ClientesProdutos/rest/api/model/ClientesModel.java
+++ Clientes-Produtos-rest-api/src/main/java/com/fiap/ClientesProdutos/rest/api/model/ClientesModel.java
@@ -5,6 +5,7 @@ import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.SequenceGenerator;
 import jakarta.persistence.Table;
 
 @Entity
@@ -12,7 +13,8 @@ import jakarta.persistence.Table;
 public class ClientesModel {
 	
 	@Id
-	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	@GeneratedValue(strategy = GenerationType.SEQUENCE, generator = "tb_clientes_seq")
+	@SequenceGenerator(name = "tb_clientes_seq", sequenceName = "tb_clientes_seq", allocationSize = 1)
 	@Column(name = "ID_CLIENTE")
 	private Long id;
 	
