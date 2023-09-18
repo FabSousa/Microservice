bf34f290faafe19a1dc80c2a0f47279c9c38a5f2
Fábio Sousa <73472338+FabSousa@users.noreply.github.com>
2023-09-18 11:43:41 -0300

    Testes de Integração (erro no no insert)

--- 2-Semestre/ms-pagamentos/src/main/resources/data.sql
+++ 2-Semestre/ms-pagamentos/src/main/resources/data.sql
@@ -4,15 +4,9 @@ pedido_id, forma_de_pagamento_id ) VALUES(1200, 'Nicodemus', '123456987', '123',
 INSERT INTO tb_pagamento(valor, nome, numero_do_cartao, codigo, validade, status,
 pedido_id, forma_de_pagamento_id ) VALUES(500.5, 'Amadeus', '987456321', '321',
 '01/27', 'CRIADO', 2, 2);
-
---INSERT INTO tb_pedido(data_hora, status) VALUES('2023-07-19', 'REALIZADO');
---INSERT INTO tb_item_do_pedido(quantidade, descricao, pedido_id) VALUES(2, 'água
---com gás', 1);
---INSERT INTO tb_item_do_pedido(quantidade, descricao, pedido_id) VALUES(1, 'pizza',
---1);
-
---INSERT INTO tb_pedido(data_hora, status) VALUES('2023-07-19', 'REALIZADO');
---INSERT INTO tb_item_do_pedido(quantidade, descricao, pedido_id) VALUES(2, 'água
---com gás', 1);
---INSERT INTO tb_item_do_pedido(quantidade, descricao, pedido_id) VALUES(1, 'pizza',
---1);
+INSERT INTO tb_pagamento(valor, nome, numero_do_cartao, codigo, validade, status,
+pedido_id, forma_de_pagamento_id ) VALUES(500.5, 'Amadeus', '987456321', '321',
+'01/27', 'CONFIRMADO', 2, 2);
+INSERT INTO tb_pagamento(valor, status, pedido_id, forma_de_pagamento_id) VALUES(125, 'CRIADO', 3, 1);
+INSERT INTO tb_pagamento(valor, status, pedido_id, forma_de_pagamento_id) VALUES(125, 'CANCELADO', 3, 1);
+INSERT INTO tb_pagamento(valor, status, pedido_id, forma_de_pagamento_id) VALUES(3255, 'CRIADO', 4, 1);
